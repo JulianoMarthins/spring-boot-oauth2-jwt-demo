61326a520a7f4665e226a50bf22f505c816ccbf8
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-03-24 11:24:24 -0300

    OAuth2
    Criação da classe de configuração.
    
    Testes realizados com sucesso no postman

---
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+
+        // http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    /*
+            Quando criada a dependência Maven para a segurança "OAuth2", automaticamente todas as rotas serão
+            restritas no sistema, para liberadas é preciso criar este arquivo de configuração, a mesma está na terceira
+            linha da função filterChain.
+
+            A primeira linha da função desabilita a proteção do tipo "CSRF", este ataca pode gravar dados na sessão,
+            está é uma vulnerabilidade onde o invasor pode acessar informações indevidas no sistema.
+
+            A API que estamos construíndo disponibiliza dados controlados e não é capaz de guardar dados em sessão,
+            o tornando assim, inacessível a este tipo de ataque.
+
+            A segunda linha da função tem a mesma função da primeira, está comentada para causar inatividade, pois
+            quero mantê-la para fins didáticos
+
+            A terceira linha da função é uma autorização para a configuração dos end-points criados por mim nesta API,
+            nesta estou permitindo que todas as requisições tenham acesso permitido, somente na classe dos controladores
+            que haverá as configurações diretas em cada rota, limitando acesso às rotas específicas.
+     */
+
+
+
+}
